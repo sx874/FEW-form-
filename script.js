a92ae5a5019c3874316c397a9afab8681f58ce68
sx874 <93100053+sx874@users.noreply.github.com>
2022-11-01 21:37:58 -0400

    janky

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 
-/* const bttn = document.getElementById("submit");
-bttn.addEventListener("click", story() {
-    document.getElementById("result").innerText = "test";
-}); */
-
-
-/* function story() {
+const bttn = document.getElementById("submit");
+bttn.addEventListener('click', story => {
     const nameAns = document.getElementById("name").value;
     const bday = document.getElementById("birthday").value;
     const address = document.getElementById("address").value;
     const SSN = document.getElementById("SSN").value;
-    document.getElementById("result").innerText = `${nameAns} is a FOOL. \n I know where you live... it's ${address} \n oh your birthday?? its ${bday} \n brb gonna go steal your identity with these little numbers \n ${SSN}`;
+    document.getElementById("result").innerText = `${nameAns} is a FOOL. \n I know where you live... it's ${address} \n what a lame birthday you have btw \n ${bday}??? what kinda birth day is that \n brb gonna go steal your identity with these little numbers \n ${SSN}`;
+//clearing inputs
+    document.getElementById("SSN").value = '';
+    document.getElementById("address").value = '';
+    document.getElementById("birthday").value = '';
+    document.getElementById("name").value = '';
+//changing css after button clicked
+    document.getElementById("form").style.fontSize = "10px";
+    document.getElementById("form").style.color = "red";
+//animate button after clicked
+    const movement = 10;
+    const circle = getElementById("animation");
+    const moveCircle = () => {
+        const xCircle = parseFloat(getComputedStyle(circle).left);
+        circle.style.left = (xCircle + movement) + "px";
+        requestAnimationFrame(moveCircle);
+    };
+    requestAnimationFrame(moveCircle);
     
-} */
-
-
-/* document.getElementById("submit").addEventListener("click", () => {
-    document.getElementById("result").innerText = "test";
-    alert("TEST!");
-}); */
-
-/* doesn't work */
-/* const bttn = document.getElementById("submit");
-bttn.addEventListener('click', story);
-function story() {
-    document.getElementById('result').innerText = "test";
-
-} */
-
-const bttn = document.getElementById("submit");
-bttn.addEventListener('click', () => {
-    document.getElementById('result').innerText = "test";
 });
+
+bttn.addEventListener('click', () => {
+    setTimeout(() => {
+        bttn.removeEventListener('click', story);
+    }, 1000)
+})
+
